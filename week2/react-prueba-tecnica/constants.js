a3f3eac720540f699cc457ad7ea36891737b7541
Raúl Fabra <fabraraul@yahoo.com>
2024-03-24 23:54:36 +0100

    5 proyecto de react. Configuramos proyecto y añadimos Readme

--- week2/react-prueba-tecnica/constants.js
+++ week2/react-prueba-tecnica/constants.js
@@ -1,2 +1,4 @@
 export const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 export const CAT_ENDPOINT_RANDOM_IMAGE = 'https://cataas.com/cat/says/'
+export const CAT_PREFIX_IMAGE_URL = 'https://cataas.com/cat/says/'
+export const LOCALHOST_URL = 'http://localhost:5173/'
--- week2/react-prueba-tecnica/tests/example.spec.js
+++ week2/react-prueba-tecnica/tests/example.spec.js
@@ -1,8 +1,6 @@
 // @ts-check
 import { test, expect } from '@playwright/test'
-
-const CAT_PREFIX_IMAGE_URL = 'https://cataas.com/cat/says/'
-const LOCALHOST_URL = 'http://localhost:5173/'
+import { CAT_PREFIX_IMAGE_URL, LOCALHOST_URL } from '../constants'
 
 test('app shows random fact and image', async ({ page }) => {
   await page.goto(LOCALHOST_URL)
