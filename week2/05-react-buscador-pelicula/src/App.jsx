6605ab2ffb97483017b059e212b70aad09edc3f6
Raúl Fabra <fabraraul@yahoo.com>
2024-03-25 18:05:40 +0100

    Terminamos prueba tecnica!!

--- week2/05-react-buscador-pelicula/src/App.jsx
+++ week2/05-react-buscador-pelicula/src/App.jsx
@@ -1,22 +1,42 @@
 import './App.css'
-import { useMovies } from './hooks/useMovies'
 import { Movies } from './components/Movies'
+import { useMovies } from './hooks/useMovies'
 import { useQuery } from './hooks/useQuery'
+import { useCallback, useState } from 'react'
+import debounce from 'just-debounce-it'
 
 function App () {
+  const [sort, setSort] = useState(false)
+
   const { query, setQuery, error } = useQuery()
-  const { movies, getMovies } = useMovies({ query })
+  const { movies, getMovies } = useMovies({ query, sort })
+
+  const debouncedGetMovies = useCallback(
+    debounce(query => {
+      console.log('query', query)
+      getMovies({ query })
+    }, 300)
+    , [])
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    getMovies()
+    getMovies({ query })
+  }
+
+  const handleSort = () => {
+    setSort(!sort)
   }
 
   const handleChange = (event) => {
     const newQuery = event.target.value
-    if (newQuery.startsWith(' ')) return
+    if (newQuery.startsWith(' ')) return undefined
     setQuery(newQuery)
     // Validacion se podría, pero esta vez esta dentro de un useEffect en un costumHook
+    // Ahora queremos que mientras escribimos en nuestra búsqueda vaya sacando las películas 👇
+    // getMovies({ query: newQuery })
+    // El problema de tener esto así sin hacer un debounce, es que vamos a estar buscando peliculas en cada letra que escribamos y se va a volver loco por que cada fetching necesita un tiempo de carga, y si se introduce a una alta velocidad el nombre de la pelicula te sacara resultados antiguos.
+    // Un debounce sería, vamos a dejar que el usuario escriba, no vamos hacer nada y esperaremos X-ms, para que la ultima llamada sea la que verdaderamente se vaya a disparar.👇 👇
+    debouncedGetMovies(newQuery)
   }
 
   return (
@@ -25,6 +45,7 @@ function App () {
         <h1>Buscador de peliculas</h1>
         <form onSubmit={handleSubmit}>
           <input style={{ border: '1px solid transparent', borderColor: error ? 'red' : 'transparent' }} onChange={handleChange} value={query} name='query' placeholder='text' />
+          <input type='checkbox' onChange={handleSort} checked={sort} />
           <button type='submit'>Buscar</button>
         </form>
         {error && <p style={{ color: 'red' }}>{error}</p>}
--- week2/05-react-buscador-pelicula/src/hooks/useMovies.js
+++ week2/05-react-buscador-pelicula/src/hooks/useMovies.js
@@ -1,13 +1,13 @@
-import { useRef, useState } from 'react'
+import { useRef, useState, useMemo, useCallback } from 'react'
 import { searchMovies } from '../services/movies'
 
-export function useMovies ({ query }) {
+export function useMovies ({ query, sort }) {
   const [movies, setMovies] = useState([])
   const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(null)
+  const [, setError] = useState(null)
   const previousSearch = useRef(query)
 
-  const getMovies = async () => {
+  const getMovies = useCallback(async ({ query }) => {
     if (query === previousSearch.current) return
 
     try {
@@ -21,9 +21,19 @@ export function useMovies ({ query }) {
     } finally {
       setLoading(false)
     }
-  }
+  }, [])
 
-  return { movies, getMovies, loading }
+  const sortedMovies = useMemo(() => {
+    return sort
+      ? [...movies].sort((a, b) => a.title.localeCompare(b.title))
+      : movies
+  }, [sort, movies])
+
+  return { movies: sortedMovies, getMovies, loading }
 }
 
 /* Como se puede ver, esta funcion MAPPEDMOVIES no se abre mediante llaves y si mediante parentesis. Esto es así porque de esta manera no hace falta usar el return. la variable con el interrogante es una manera de condicionar, en el ejemplo, si existe movies hazme el mapeo; si no, no devuelvas nada. */
+
+/* Tambien vamos a ver los hooks useMemo y useCallback. Estos se utilizan para que, aquellas funciones que, hacen una llamada a la api por ejemplo y nos muestra los datos no se renderice cada vez que hay un cambio en algun estado en particular que NO AFECTA al renderizado de los datos. En otras palabras, el useMemo SIRVE para que no vuelva a llamar a la funcion en situaciones donde no nos interesa un renderizado continuo porque no esta afectando para nada al resultado de los datos que estamos viendo y SOLAMENTE se vuelvan a renderizar en el momento que SI CAMBIEMOS el estado de un elemento que influye en el cambio de los datos de la API. En resumen, con este hook lo que hacemos es memorizar un valor, lo estamos guardando y solo se ejecuta cuando cambiemos el estado de un elemento que nos haga cambiar el renderizado de los datos. */
+
+/* El tema del useCallback es como El useMemo porque por detras trabaja como useMemo pero este no hace falta llamar a una funcion para que llame a otra funcion. Con el uso de useCallback directamente llamamos a la funcion asincrona. */
